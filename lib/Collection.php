9d8351a53a17d5a50b775c7747bda9725a2ccd21
wnowicki <wonowicki@gmail.com>
2015-05-26 22:06:51 +0100

    Collection

---
+++ lib/Collection.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the WNowicki/Collections package.
+ *
+ * (c) WNowicki <dev@wojciechnowicki.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WNowicki\Collections;
+
+/**
+ * Collection
+ *
+ * @author WN
+ * @package WNowicki\Collections
+ */
+class Collection extends AbstractCollection
+{
+    /**
+     * Checks if given element is valid ype for this collection
+     *
+     * @param $element
+     * @return bool
+     */
+    protected function isValid($element)
+    {
+        return true;
+    }
+}
